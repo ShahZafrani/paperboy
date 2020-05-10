9c5a6ae5581150cacb1f31774748c03dccc7da3b
Shah Zafrani <shahzafrani@gmail.com>
2020-05-10 16:19:48 -0400

    removed debug code. no longer init-ing and clearing display each refresh

--- image.py
+++ image.py
@@ -5,7 +5,6 @@ fontfile = './fonts/Roboto-Medium.ttf'
 datefontfile = './fonts/Roboto-Medium.ttf'
 weatherfontfile = './fonts/Font Awesome 5 Free-Solid-900.otf'
 offset = 3
-debug = False
 
 from PIL import Image
 from PIL import ImageDraw
@@ -14,9 +13,7 @@ import datetime
 import time
 import fuzzytime
 import weather
-
-if debug == False:
-    import epd7in5
+import epd7in5
 # import imagedata
 
 EPD_WIDTH = 640
@@ -36,7 +33,7 @@ WBOUNDS_TOP = 40
 TEMPBOUNDS_LEFT = 480
 TEMPBOUNDS_TOP = 190
 
-def draw_clock(now): 
+def draw_clock(now, epd): 
     try:
         weather_response_code, weather_json = weather.getWeather(cityname, openweather_apikey)
     except:
@@ -50,15 +47,6 @@ def draw_clock(now):
         weather_temp = "gg"+ u"\u00B0"
     timetext = fuzzytime.updateTime(offset, now)
     datetext = fuzzytime.getDate(now)
-    if debug == False:
-        epd = epd7in5.EPD()
-        epd.init()
-        #epd.Clear()
-        print("epd init complete")
-    else: 
-        print(line1)
-        print(line2)
-        print(line3)
     image = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 255)
     draw = ImageDraw.Draw(image)
     font = ImageFont.truetype(fontfile, TEXT_FONT_SIZE)
@@ -69,27 +57,24 @@ def draw_clock(now):
     draw.text((DATE_BOUNDS_LEFT, DATE_BOUNDS_TOP), datetext, fill=0, font=datefont)
     draw.text((WBOUNDS_LEFT, WBOUNDS_TOP), weather_icon, font = wfont, fill = 0)
     draw.text((TEMPBOUNDS_LEFT, TEMPBOUNDS_TOP), weather_temp, font = font, fill = 0)
-    if debug == False:
-        epd.display(epd.getbuffer(image))
-    else:
-        image.save('./images/test.bmp')
+    epd.display(epd.getbuffer(image))
 
-def main():
-    if debug == False:
-        lastMinuteText = "forever"
-        while True:
-            now = datetime.datetime.now()
-            minuteText = fuzzytime.updateTime(offset, now)[0]
-            if (minuteText != lastMinuteText):
-                lastMinuteText = minuteText
-                # draw
-                draw_clock(now)
-            # check for a re-draw every minute, after the offset is reached, this should only trigger every five minutes
-            time.sleep(60)
-    else:
-        draw_clock(datetime.datetime.now())
+def main(epd):
+    lastMinuteText = "forever"
+    while True:
+        now = datetime.datetime.now()
+        minuteText = fuzzytime.updateTime(offset, now)[0]
+        if (minuteText != lastMinuteText):
+            lastMinuteText = minuteText
+            # draw
+            draw_clock(now, epd)
+        # check for a re-draw every minute, after the offset is reached, this should only trigger every five minutes
+        time.sleep(60)
 
 
 
 if __name__ == '__main__':
-    main()
+    epd = epd7in5.EPD()
+    epd.init()
+    epd.Clear()
+    main(epd)
