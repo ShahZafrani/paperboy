71e373d20fb144801ca6701defdd05b4c9e08e21
Shah Zafrani <shahzafrani@gmail.com>
2020-10-30 01:58:00 -0400

    fixed bug. added phase text. inverted colors

--- image.py
+++ image.py
@@ -17,6 +17,9 @@ from util import moonphase
 from util import fuzzytime
 from util import csv_util
 
+BACKDROP_COLOR = 0 # black
+FILL_COLOR = 255 # white
+
 EPD_WIDTH = 800
 EPD_HEIGHT = 480
 TEXT_FONT_SIZE = 36
@@ -32,6 +35,8 @@ DATE_BOUNDS_LEFT = 660
 MOON_BOUNDS_TOP = 40
 MOON_BOUNDS_LEFT = 50
 MOON_FONT_SIZE = 384
+PHASE_BOUNDS_LEFT = MOON_BOUNDS_LEFT + 100
+PHASE_BOUNDS_TOP = DATE_BOUNDS_TOP
 
 
 # TODO: parameterize weather and time/date text
@@ -43,26 +48,33 @@ def createImage(now):
     today_times = csv_util.get_salat_times(now.day, filepath)
 
     # image setup
-    image = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 255)
+    image = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), BACKDROP_COLOR)
     draw = ImageDraw.Draw(image)
     font = ImageFont.truetype(fontfile, TEXT_FONT_SIZE)
     datefont = ImageFont.truetype(datefontfile, DATE_FONT_SIZE)
     moonfont = ImageFont.truetype(moonfontfile, MOON_FONT_SIZE)
     # image draw
     text = "fajr {}\n duhr {}\n asr {}\n maghrib {}\n isha {}".format(today_times[1], today_times[3], today_times[4], today_times[5], today_times[6])
-    draw.multiline_text((BOUNDS_LEFT, BOUNDS_TOP), text, fill=0, font=font, anchor=None, spacing=TIME_OFFSET, align="right")
+    draw.multiline_text((BOUNDS_LEFT, BOUNDS_TOP), text, fill=FILL_COLOR, font=font, anchor=None, spacing=TIME_OFFSET, align="right")
     # draw.text((BOUNDS_LEFT, BOUNDS_TOP), "fajr {}".format(today_times[1]), fill=0, font=font, direction="rtl")
     # draw.text((BOUNDS_LEFT, BOUNDS_TOP + TIME_OFFSET), "duhr {}".format(today_times[3]), fill=0, font=font, direction="rtl")
     # draw.text((BOUNDS_LEFT, BOUNDS_TOP + TIME_OFFSET*2), "asr {}".format(today_times[4]), fill=0, font=font, direction="rtl")
     # draw.text((BOUNDS_LEFT, BOUNDS_TOP + TIME_OFFSET*3), "maghrib {}".format(today_times[5]), fill=0, font=font, direction="rtl")
     # draw.text((BOUNDS_LEFT, BOUNDS_TOP + TIME_OFFSET*4), "isha {}".format(today_times[6]), fill=0, font=font, direction="rtl")
     
-    draw.text((DATE_BOUNDS_LEFT, DATE_BOUNDS_TOP), datetext, fill=0, font=datefont)
-
-    draw.text((MOON_BOUNDS_LEFT, MOON_BOUNDS_TOP), moonphase.getMoonPhaseChar(now), fill=0, font=moonfont)
+    moon_phase_char, moon_phase_text = moonphase.getMoonPhase(now)
+    draw.text((DATE_BOUNDS_LEFT, DATE_BOUNDS_TOP), datetext, fill=FILL_COLOR, font=datefont)
+    draw.text((MOON_BOUNDS_LEFT, MOON_BOUNDS_TOP), moon_phase_char, fill=FILL_COLOR, font=moonfont)
+    draw.text((PHASE_BOUNDS_LEFT, PHASE_BOUNDS_TOP), moon_phase_text, fill=FILL_COLOR, font=datefont)
     # draw.ellipse((40, 40, 400, 400), fill = 0, outline =0)
     return image
 
 if __name__ == '__main__':
-        nowImage = createImage(datetime.datetime.now())
-        nowImage.save('./images/now.bmp')
+    nowImage = createImage(datetime.datetime.now())
+    nowImage.save("./images/now.bmp")
+    for i in range(1, 30):
+        phase = i / 30.0
+        print(i)
+        nowImage = createImage(datetime.datetime.now() + datetime.timedelta(days=i))
+        nowImage.save("./images/nowplus_{}.bmp".format(i))
+    
--- util/moonphase.py
+++ util/moonphase.py
@@ -1,9 +1,40 @@
 from . import pyorbital_moon_phase
 import string 
-def getMoonPhaseChar(now):
-    lowercase = list(string.ascii_lowercase)
+import datetime
+
+fullThreshold = 0.985
+newThreshold = 0.015
+
+waxing_phase_chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm']
+waning_phase_chars = ['z', 'y', 'x', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n']
+
+def getMoonPhase(now):
     currentPosition = pyorbital_moon_phase.moon_phase(now)
-    position_index = int(round(currentPosition * 26))
-    if position_index == 26:
-        position_index = 25
-    return lowercase[position_index]
+    if currentPosition >= fullThreshold:
+        return "0", "full moon"
+    if currentPosition <= newThreshold:
+        return "1", "new moon"
+    yesterdayPosition = pyorbital_moon_phase.moon_phase(now - datetime.timedelta(days=1))
+    if (yesterdayPosition >= currentPosition):
+        direction = "waning"
+        char_pos = int (round(currentPosition * len(waning_phase_chars)))
+        if (char_pos == len(waning_phase_chars)):
+            char_pos -=1
+        char = waning_phase_chars[char_pos]
+    elif (yesterdayPosition < currentPosition):
+        direction = "waxing"
+        char_pos = int (round(currentPosition * len(waxing_phase_chars)))
+        if (char_pos == len(waxing_phase_chars)):
+            char_pos -=1
+        char = waxing_phase_chars[char_pos]
+    if (char == "g"):
+        phase_text = "first quarter"
+    elif (char == "t"):
+        phase_text = "last quarter"
+    elif (currentPosition < 0.50):
+        phase_text = direction + " crescent"
+    else:
+        phase_text = direction + " gibbous"
+    return char, phase_text
+    
+    
